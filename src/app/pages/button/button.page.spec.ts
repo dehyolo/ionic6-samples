137a6a268015e421da16e1672a691331b105a739
André Lopes <yonolino@live.com>
2022-03-13 20:30:01 -0300

    preparation

--- src/app/pages/button/button.page.spec.ts
+++ src/app/pages/button/button.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { FolderPage } from './folder.page';
 
-describe('FolderPage', () => {
-  let component: FolderPage;
-  let fixture: ComponentFixture<FolderPage>;
+import { ButtonPage } from './button.page';
+
+describe('ButtonPage', () => {
+  let component: ButtonPage;
+  let fixture: ComponentFixture<ButtonPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ FolderPage ],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+      declarations: [ ButtonPage ],
+      imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(FolderPage);
+    fixture = TestBed.createComponent(ButtonPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
